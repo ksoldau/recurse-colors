da8e297024c7314091aaac23e1e8772dbd80eb0b
Katie Soldau <ksoldau@Katies-MacBook-Air.local>
2026-07-23 17:42:41 -0600

    form logic

--- main.js
+++ main.js
@@ -15,8 +15,48 @@ function generateRandomHslColor() {
 }
 
 let currColor = generateRandomHslColor()
+let guessCorrectness = {
+    h: undefined,
+    s: undefined, 
+    l: undefined,
+} 
+
 
 document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById("hsl_form");
     const swatch = document.getElementById("swatch")
     swatch.style.backgroundColor = createHslColor(currColor)
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault()
+
+        const hGuess = document.getElementById('h').value
+        const sGuess = document.getElementById('s').value
+        const lGuess = document.getElementById('l').value
+        
+        let alertMsg = ''
+        if (Number(hGuess) !== Number(currColor.h)) {
+            alertMsg += `Wrong h. Was ${currColor.h} got ${hGuess}\n`
+        } else {
+            alertMsg += `Correct! Hue was ${currColor.h}`
+        }
+        if (Number(sGuess) !== Number(currColor.s)) {
+            alertMsg += `Wrong s. Was ${currColor.s} got ${sGuess}\n`
+        } else {
+            alertMsg += `Correct! Saturation was ${currColor.s}`
+        }
+        if (Number(lGuess) !== Number(currColor.l)) {
+            alertMsg += `Wrong l. Was ${currColor.l} got ${lGuess}\n`
+        } else {
+            alertMsg += `Correct! Lightness was ${currColor.l}`
+        }
+
+        alert(alertMsg)
+    })
+
+    form.addEventListener("input", () => {
+        const button = document.getElementById("submit")
+        button.disabled = !form.checkValidity();
+    });
 })
+
