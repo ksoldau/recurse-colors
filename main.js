925d0af96f9c62c59b8af20f6dba10bca00d9754
Katie Soldau <ksoldau@Katies-MacBook-Air.local>
2026-07-23 17:11:55 -0600

    rest of init commit

---
+++ main.js
@@ -0,0 +1,22 @@
+function createHslColor({h, s, l}) {
+    return  `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+function generateRandomHslColor() {
+    const h = Math.floor(Math.random() * 361); // 0 to 360
+    const s = Math.floor(Math.random() * 101); // 0 to 100
+    const l = Math.floor(Math.random() * 101); // 0 to 100
+
+    return {
+        h, 
+        s, 
+        l
+    }
+}
+
+let currColor = generateRandomHslColor()
+
+document.addEventListener('DOMContentLoaded', () => {
+    const swatch = document.getElementById("swatch")
+    swatch.style.backgroundColor = createHslColor(currColor)
+})
